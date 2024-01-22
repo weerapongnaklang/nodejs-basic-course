5baaff403b4ee5e444a62d648730804942cf3557
Weerapong Naklang <naklangweerapong@gmail.com>
2024-01-22 09:37:24 +0700

    delete content

--- 01-node-intro/exercises/01-04-more-cli-options/index.js
+++ 01-node-intro/exercises/01-04-more-cli-options/index.js
@@ -5,14 +5,6 @@ const filePath = "data.txt";
 const option = process.argv[2];
 const input = process.argv[3];
 
-// if (option === "w") {
-//   fs.appendFileSync(filePath, input + "\n");
-//   console.log("appended:", input);
-// } else {
-//   console.log("invalid option");
-// }
-// --------------------vvvv-----------------
-
 if (option === "w") {
   fs.appendFileSync(filePath, input + "\n");
   console.log("appended:", input);
